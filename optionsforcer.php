148399f908db304a926ad0d1d5b926b1a3fdbb09
Matheus E. Muller <hello@memuller.com>
2017-09-29 17:41:20 -0300

    Adds PHPDoc comments

--- optionsforcer.php
+++ optionsforcer.php
@@ -11,10 +11,21 @@
 
 namespace OptionsForcer;
 
+// fails if called outside of Wordpress.
 if(!function_exists('\add_action')) die();
 
+/**
+ * Class OptionsForcer\Plugin
+ * The main (and only) plugin class.
+ */
 class Plugin {
-
+  /**
+   * Hooks the plugin.
+   * Checks if a global $wp_options is defined and is an array,
+   * if so, overrides the options named there by calling overrideOption.
+   *
+   * @uses self::overrideOption
+   */
   public static function init(){
     global $wp_options;
     if ( isset($wp_options) && is_array($wp_options) ){
@@ -24,6 +35,13 @@ class Plugin {
     }
   }
 
+  /**
+   * Overrides a wp_option w/ a given value,
+   * by hooking at the option_$NAME filter.
+   *
+   * @param string $name The option to override.
+   * @param void $value The value which will override the option.
+   */
   public static function overrideOption($name, $value){
     add_filter("option_${name}", function() use($value) {
       return $value;
@@ -32,4 +50,5 @@ class Plugin {
 
 }
 
+// Calls the plugin main hook.
 add_action('init', ['\OptionsForcer\Plugin', 'init']);
